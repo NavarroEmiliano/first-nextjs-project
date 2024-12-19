23393bad8e77e9cbe56ddd10471423ff676fbf65
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-12-19 02:46:13 -0300

    gap

--- src/app/components/UserCardInfo.tsx
+++ src/app/components/UserCardInfo.tsx
@@ -1,14 +1,14 @@
 const UserCardInfo = () => {
   return (
-    <article className="flex flex-wrap rounded-2xl bg-sky-900 p-4">
+    <article className="flex flex-wrap rounded-2xl bg-sky-900 p-8 gap-8">
       <div className="h-20 w-20 min-w-20 rounded-full bg-sky-500"></div>
-        <div className="text-white md:pl-4">
-          <p>The Octocat</p>
-          <p>@octocat</p>
-          <p>Joined 25 Jan 2011</p>
-        </div>
-      <div className="md:pl-24">
-        <p>
+      <div className="flex flex-col justify-between pl-4 text-white md:flex-row">
+        <p className="text-2xl font-semibold">The Octocat</p>
+        <p>@octocat</p>
+        <p>Joined 25 Jan 2011</p>
+      </div>
+      <div className="flex flex-col gap-8 md:pl-24">
+        <p className="tracking-wider">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima qui
           adipisci fuga tenetur debitis quas odio vel! Dicta nam dolor incidunt
           corrupti nihil voluptatum sapiente, quod dolores, hic ex tempora?
