6e8f30a695b3ef044db9385550ac7ff01b357907
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-12-19 03:03:48 -0300

    responsive

--- src/app/components/UserCardInfo.tsx
+++ src/app/components/UserCardInfo.tsx
@@ -1,19 +1,22 @@
 const UserCardInfo = () => {
   return (
-    <article className="flex flex-wrap rounded-2xl bg-sky-900 p-8 gap-8">
-      <div className="h-20 w-20 min-w-20 rounded-full bg-sky-500"></div>
-      <div className="flex flex-col justify-between pl-4 text-white md:flex-row">
-        <p className="text-2xl font-semibold">The Octocat</p>
-        <p>@octocat</p>
-        <p>Joined 25 Jan 2011</p>
+    <article className="flex flex-wrap gap-y-8 rounded-2xl bg-sky-900 p-8">
+      <div className="flex flex-auto">
+        <div className="h-20 w-20 min-w-20 flex-none rounded-full bg-sky-500"></div>
+        <div className="flex-auto justify-between pl-4 text-white md:grid md:grid-cols-2">
+          <p className="text-2xl font-semibold">The Octocat</p>
+          <p className="md:text-right">@octocat</p>
+          <p>Joined 25 Jan 2011</p>
+        </div>
       </div>
+
       <div className="flex flex-col gap-8 md:pl-24">
         <p className="tracking-wider">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima qui
           adipisci fuga tenetur debitis quas odio vel! Dicta nam dolor incidunt
           corrupti nihil voluptatum sapiente, quod dolores, hic ex tempora?
         </p>
-        <div className="flex justify-around border p-2 text-white">
+        <div className="flex justify-around rounded-xl bg-blue-950 p-4 text-white">
           <article>
             <p>Repos</p>
             <p className="font-bold">8</p>
@@ -27,7 +30,7 @@ const UserCardInfo = () => {
             <p className="font-bold">9</p>
           </article>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col md:grid md:grid-cols-2 gap-2">
           <p>San Francisco</p>
           <p>https://github.blog</p>
           <p>Not Available</p>
